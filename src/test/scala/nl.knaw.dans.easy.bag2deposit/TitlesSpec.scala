5825ca7fb37c04a5b8711b43a86a8f24d0b956b2
jo-pol <jo-pol@users.noreply.github.com>
2021-01-08 10:47:27 +0100

    attempt to find archeo report numbers

--- src/test/scala/nl.knaw.dans.easy.bag2deposit/TitlesSpec.scala
+++ src/test/scala/nl.knaw.dans.easy.bag2deposit/TitlesSpec.scala
@@ -14,17 +14,28 @@ class TitlesSpec extends AnyFlatSpec {
           titles
             .replaceAll(""""(.*)"""", "$1") // strip quotes
             .replaceAll("""\\,""", "%") // replace real commas
-            .split(",")
+            .split("""(,|\\n *)""")
             .map(_.replaceAll("%", ",")) // restore real commas
+            .filter(_.trim.nonEmpty)
+            .sortBy(identity)
+            .distinct // e.g. easy-dataset:34135, easy-dataset:99840
             .toSeq
         )
       }.toMap
+
+    // a special single one easy-dataset:99840 "Arcadis Archeologische Rapporten [2017 - 116]"
+    val nrRegExp = " [-_/a-z0-9]+\\.? *$"
+
+    // easy-dataset:103605,"...Zuidnederlandse Archeologische Rapporten 62..." heeft al een identifier: "ZAR 62"
+
+    val mnemonics = "adc|baac|gra|mug|vestigia|zan|zar"
+    val prefix = s"(ar(ch|g)a?eo[a-z]*|$mnemonics)"
+    val suffix = "([a-z]*rapport|[a-z]*notitie|bericht)[a-z]*"
     val rapporten = titles.values.toSeq
-      .flatMap(_
-        .map(_.toLowerCase.replaceAll(" [-_/a-z0-9.]+$", "")) // drop the supposed number at the tail
-        .filter(str => !str.startsWith("thematische collectie: ")
-          && str.matches(".*arch.*(rapport[a-z]+|notitie[a-z]+).*")
-        )
+      .flatMap(title => title
+        .filter(_.matches(s".*$nrRegExp"))
+        .map(_.toLowerCase.replaceAll(nrRegExp, "").replace(" [2017 - 116]", ""))
+        .filter(_.matches(s"(.*$prefix[ -]$suffix.*|$mnemonics)"))
       ).sortBy(identity)
       .distinct
 
