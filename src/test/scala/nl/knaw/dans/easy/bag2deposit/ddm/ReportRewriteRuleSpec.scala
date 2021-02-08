eef8f9b349149d6cc3cd4b539c4096b0dba45656
jo-pol <jo-pol@users.noreply.github.com>
2021-02-08 14:13:13 +0100

    moved part of RewriteSpec to ReportRewriteRuleSpec

--- src/test/scala/nl/knaw/dans/easy/bag2deposit/ddm/ReportRewriteRuleSpec.scala
+++ src/test/scala/nl/knaw/dans/easy/bag2deposit/ddm/ReportRewriteRuleSpec.scala
@@ -6,6 +6,8 @@ import nl.knaw.dans.easy.bag2deposit.ddm
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.xml.Node
+
 class ReportRewriteRuleSpec extends AnyFlatSpec with Matchers with FileSystemSupport {
   private val rule: ReportRewriteRule = ddm.ReportRewriteRule(File("src/main/assembly/dist/cfg"))
   private val uuidToPreferredLabel = rule.reportMap.map(r => r.uuid -> r.label).toMap
@@ -42,16 +44,53 @@ class ReportRewriteRuleSpec extends AnyFlatSpec with Matchers with FileSystemSup
     ).flatMap(id =>
       rule.transform(<identifier>{ id }</identifier>)
     )
-    transformed
-      .map(node => (node \@ "valueURI").replaceAll(".*/", ""))
-      .sortBy(identity).distinct
-      .map(uuidToPreferredLabel.getOrElse(_, ""))
-      .sortBy(identity) shouldBe
+    toPreferredLabel(transformed) shouldBe
       Seq("", "Archol-rapport", "Argo-rapport", "KSP-rapport", "Periplus / Archeomare Rapport", "Rapport")
 
     transformed.map(node => (node \@ "reportNo")) shouldBe
       List("152", "127", "183", "18382", "A07_A010")
   }
+
+  private def toPreferredLabel(transformed: Seq[Node]) = {
+    transformed
+      .map(node => (node \@ "valueURI").replaceAll(".*/", ""))
+      .sortBy(identity).distinct
+      .map(uuidToPreferredLabel.getOrElse(_, ""))
+      .sortBy(identity)
+  }
+
+  "titles" should "not change" in {
+    val input = Seq(
+      <title>blabla</title>,
+      <title>blablabla : rapport 21</title>,
+      <alternative>rabarbera</alternative>,
+      <alternative>rabarbera : rapport 21</alternative>,
+    )
+    input.flatMap(rule.transform) shouldBe input
+  }
+
+  it should "return proper numbers" in {
+    // difference between titles in the profile/dcmiMedata section are tested with RewriteSpec
+    val results = Seq(
+      <alternative>rapportnr.: 123</alternative>,
+      <title>Rapport 456</title>,
+      <alternative>Transect-rapport 2859: Een Archeologisch Bureauonderzoek. Ellecom, glasvezeltracé Eikenstraat, Gemeente Rheden (GD).</alternative>,
+    ).flatMap(rule.transform)
+
+    val transformed = results.filter(_.label == "reportNumber")
+    transformed
+      .map(node => node \@ "reportNo")
+      .sortBy(identity) shouldBe
+      Seq("123", "2859", "456")
+    transformed.map(_.text).sortBy(identity) shouldBe
+      Seq("Rapport 456", "Transect-rapport 2859", "rapportnr.")
+    results.filter(_.label != "reportNumber").map(_.text) shouldBe
+      Seq(
+        "rapportnr.: 123", // TODO why treated like <report-nr> : <rest-of-title> ?
+        "Transect-rapport 2859: Een Archeologisch Bureauonderzoek. Ellecom, glasvezeltracé Eikenstraat, Gemeente Rheden (GD).",
+      )
+  }
+
   it should "produce identifier checklists" in {
     val results = identifiers.flatMap(id =>
       rule.transform(<identifier>{ id }</identifier>)
@@ -77,7 +116,7 @@ class ReportRewriteRuleSpec extends AnyFlatSpec with Matchers with FileSystemSup
       results("reportNumber").map(_.text).mkString("\n")
     )
     val missed = results("title").groupBy(title => rule.reportMap.exists(cfg =>
-      title.text.toLowerCase.matches(".*"+cfg.regexp+ReportRewriteRule.nrRegexp))
+      title.text.toLowerCase.matches(".*" + cfg.regexp + ReportRewriteRule.nrRegexp))
     )
     (testDir / "titles-missed-at-end").writeText(
       missed(true).map(_.text).mkString("\n")
