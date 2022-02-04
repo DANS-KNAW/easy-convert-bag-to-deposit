2c3b6acdb24b8ccd8858a976c32324336bec2a06
jo-pol <jo-pol@users.noreply.github.com>
2022-02-04 14:39:56 +0100

    ad hoc tool for DD-833
    last column of hardcoded input file is supposed to contain titles extracted from solr

---
+++ src/test/scala/nl.knaw.dans.easy.bag2deposit/ddm/TilesPerDataset.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2020 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.easy.bag2deposit.ddm
+
+import better.files.File
+import nl.knaw.dans.easy.bag2deposit.ddm
+import nl.knaw.dans.easy.bag2deposit.ddm.ReportRewriteRule.nrRegexp
+
+object TilesPerDataset extends App {
+  File("data/titles.tsv")
+    .lines.toSeq
+    .map(_.split("\t"))
+    .foreach { a =>
+      a.last
+        .replaceAll(""""(.*)"""", "$1") // strip quotes
+        .replaceAll("""\\,""", "%") // replace real commas
+        .split("""(,|\\n *)""")
+        .map(_.replaceAll("%", ",")) // restore real commas
+        .filter(_.trim.nonEmpty)
+        .sortBy(identity)
+        .distinct // e.g. easy-dataset:34135, easy-dataset:99840
+        .toSeq
+        .foreach(t => println(s"${a.init.mkString("\t")}\t $t"))
+    }
+}
