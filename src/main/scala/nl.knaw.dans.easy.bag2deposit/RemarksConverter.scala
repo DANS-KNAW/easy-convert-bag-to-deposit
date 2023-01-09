553361d37cdea60256058fd35ec78eef3c582ee6
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2023-01-09 13:14:37 +0100

    Ignore empty remarks field

--- src/main/scala/nl.knaw.dans.easy.bag2deposit/RemarksConverter.scala
+++ src/main/scala/nl.knaw.dans.easy.bag2deposit/RemarksConverter.scala
@@ -44,19 +44,23 @@ class RemarksConverter(cfgDir: File) extends DebugEnhancedLogging {
     val cat = remarksMap.getOrElse(datasetId, RemarksCategory.description)
 
     def convert(remark: NodeSeq): NodeSeq = {
-      cat match {
-        case RemarksCategory.access => <dct:accessRights>{ remark.text }</dct:accessRights>
-        case RemarksCategory.citation => <dct:bibliographicCitation>{ remark.text }</dct:bibliographicCitation>
-        case RemarksCategory.contact |
-             RemarksCategory.contributor |
-             RemarksCategory.funder => <ddm:description descriptionType="Other">{ remark.text }</ddm:description>
-        case RemarksCategory.copyright => <dct:rightsHolder>{ remark.text }</dct:rightsHolder>
-        case RemarksCategory.description => <dct:description>{ remark.text }</dct:description>
-        case RemarksCategory.files => <ddm:description descriptionType="TechnicalInfo">{ remark.text }</ddm:description>
-        case RemarksCategory.ignore => NodeSeq.Empty
-        case RemarksCategory.provenance => <dct:provenance>{ remark.text }</dct:provenance>
-        case RemarksCategory.relation => <dc:relation>{ remark.text }</dc:relation>
-        case RemarksCategory.collectiondate => <dc:date>{ remark.text }</dc:date>
+      if (remark.isEmpty || remark.text == "")
+        NodeSeq.Empty
+      else {
+        cat match {
+          case RemarksCategory.access => <dct:accessRights>{ remark.text }</dct:accessRights>
+          case RemarksCategory.citation => <dct:bibliographicCitation>{ remark.text }</dct:bibliographicCitation>
+          case RemarksCategory.contact |
+               RemarksCategory.contributor |
+               RemarksCategory.funder => <ddm:description descriptionType="Other">{ remark.text }</ddm:description>
+          case RemarksCategory.copyright => <dct:rightsHolder>{ remark.text }</dct:rightsHolder>
+          case RemarksCategory.description => <dct:description>{ remark.text }</dct:description>
+          case RemarksCategory.files => <ddm:description descriptionType="TechnicalInfo">{ remark.text }</ddm:description>
+          case RemarksCategory.ignore => NodeSeq.Empty
+          case RemarksCategory.provenance => <dct:provenance>{ remark.text }</dct:provenance>
+          case RemarksCategory.relation => <dc:relation>{ remark.text }</dc:relation>
+          case RemarksCategory.collectiondate => <dc:date>{ remark.text }</dc:date>
+        }
       }
     }
 
