f4bd053a34b6c968a9c33d55c66df1eb9b5f9cf8
lindareijnhoudt <linda.reijnhoudt@dans.knaw.nl>
2023-01-19 13:27:49 +0100

    DD-1241: Rewrite license to conform to list of allowed licenses (#114)
    Co-authored-by: Jo Pol <jo-pol@users.noreply.github.com>

---
+++ src/main/scala/nl.knaw.dans.easy.bag2deposit/ddm/LicenseRewriteRule.scala
@@ -0,0 +1,115 @@
+/*
+ * Copyright (C) 2020 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.easy.bag2deposit.ddm
+
+import better.files.File
+import nl.knaw.dans.easy.bag2deposit.parseCsv
+import nl.knaw.dans.lib.logging.DebugEnhancedLogging
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.StringUtils.isBlank
+
+import java.net.URI
+import scala.util.Try
+import scala.xml.{ NamespaceBinding, Node }
+import scala.xml.transform.RewriteRule
+
+case class LicenseRewriteRule(cfgDir: File) extends RewriteRule with DebugEnhancedLogging {
+  private val supportedLicenses: List[URI] = parseCsv(cfgDir / "supported-licenses.txt", 0).toSeq
+    .map(r => new URI(r.get(0)))
+    .toList
+
+
+  private val variantToLicense: Map[String, String] = {
+    val csvFormat = CSVFormat.RFC4180
+      .withHeader("Variant", "Normalized")
+      .withDelimiter(',')
+      .withRecordSeparator('\n')
+    parseCsv(cfgDir / "license-uri-variants.csv", nrOfHeaderLines = 1, csvFormat)
+      .map(record => record.get("Variant") -> record.get("Normalized")).toMap
+  }
+
+  override def transform(node: Node): Seq[Node] = {
+    if (! isLicenseUri(node))
+      node
+    else {
+      val correctURI = getLicenseUri(supportedLicenses, variantToLicense, node)
+      <dct:license xsi:type="dct:URI">{ correctURI.toString }</dct:license>
+    }
+  }
+
+  private def isLicenseUri(node: Node): Boolean = {
+    if (!("license" == node.label) || !("http://purl.org/dc/terms/" == node.namespace) || !hasXsiType(node, "URI")) {
+      false
+    } else {
+      // validate it is a valid URI
+      Try(new URI(node.text.trim)).isSuccess
+    }
+  }
+
+  private def hasXsiType(node: Node, xsiType: String): Boolean = {
+    val attributes = node.attributes
+    if (attributes == null) return false
+    val binding = NamespaceBinding("xsi", "http://www.w3.org/2001/XMLSchema-instance", node.scope)
+    val maybeTypes = attributes.get("http://www.w3.org/2001/XMLSchema-instance", binding, "type")
+    if ( maybeTypes.isEmpty ) return false
+    maybeTypes.exists(_.exists { item =>
+      val text = item.text
+      return xsiType == text || text.endsWith(":" + xsiType)
+    })
+
+  }
+  private def getLicenseUri(supportedLicenses: List[URI], variantToLicense: Map[String, String], licenseNode: Node): URI = {
+    val licenseText = Option(licenseNode)
+      .map(_.text)
+      .map(_.trim)
+      .map(removeTrailingSlash)
+      .map(s => variantToLicense.getOrElse(s,s))
+      .getOrElse(throw new IllegalArgumentException("License node is null"))
+
+    try {
+      if (!isLicenseUri(licenseNode))
+        throw new IllegalArgumentException("Not a valid license node")
+      var licenseUri = new URI(licenseText)
+      val licenseUriFinal = licenseUri
+      val maybeLicenseUri = normalizeScheme(supportedLicenses, licenseUri)
+      if(maybeLicenseUri.equals(Option.empty))
+        new IllegalArgumentException(String.format("Unsupported license: %s", licenseUriFinal))
+      licenseUri = maybeLicenseUri.get
+      supportedLicenses.find(v => v == licenseUri).getOrElse(throw new IllegalArgumentException(String.format("Unsupported license: %s", licenseUri)))
+    } catch {
+      case e: Exception =>
+        logger.error("Invalid license URI: {}", licenseText, e)
+        throw new IllegalArgumentException("Not a valid license URI", e)
+    }
+  }
+
+  private def removeTrailingSlash(s: String): String = {
+    if (s.endsWith("/")) return s.substring(0, s.length - 1)
+    s
+  }
+
+  private def normalizeScheme(supportedLicenses: List[URI], uri: URI): Option[URI] = {
+    val schemes: Set[String] = Set("https", "http")
+    supportedLicenses.find { license =>
+      license.getHost == uri.getHost &&
+        license.getPath == uri.getPath &&
+        license.getPort == uri.getPort &&
+        license.getQuery == uri.getQuery &&
+        schemes.contains(uri.getScheme)
+    }
+  }
+}
---
+++ src/test/scala/nl.knaw.dans.easy.bag2deposit/ddm/LicenseRewriteRuleSpec.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2020 DANS - Data Archiving and Networked Services (info@dans.knaw.nl)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.knaw.dans.easy.bag2deposit.ddm
+
+import better.files.File
+import nl.knaw.dans.easy.bag2deposit.ddm
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class LicenseRewriteRuleSpec extends AnyFlatSpec with Matchers {
+  private val rule: LicenseRewriteRule = ddm.LicenseRewriteRule(File("src/main/assembly/dist/cfg"))
+
+  "transform <license>" should "not change" in {
+    val input = Seq(
+      <dct:license xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:dct="http://purl.org/dc/terms/" xsi:type="dct:URI">
+        http://creativecommons.org/licenses/by-nc-sa/4.0</dct:license>,
+    )
+    input.flatMap(rule.transform).text shouldBe "http://creativecommons.org/licenses/by-nc-sa/4.0"
+  }
+
+ it should "change https into http" in {
+   val input = Seq(
+       <dct:license xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:dct="http://purl.org/dc/terms/" xsi:type="dct:URI">https://creativecommons.org/publicdomain/zero/1.0</dct:license>,
+   )
+   input.flatMap(rule.transform).text shouldBe "http://creativecommons.org/publicdomain/zero/1.0"
+ }
+
+  it should "be a variant" in {
+    val input = Seq(
+     <dct:license xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:dct="http://purl.org/dc/terms/" xsi:type="dct:URI">http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html</dct:license>,
+    )
+    input.flatMap(rule.transform).text shouldBe "http://www.gnu.org/licenses/old-licenses/gpl-2.0"
+  }
+}
